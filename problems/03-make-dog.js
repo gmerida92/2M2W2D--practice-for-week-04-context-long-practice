057f901b4a08175711e7525c9bdba874bae7d8da
george <gmerida92@gmail.com>
2022-03-08 18:47:19 -0800

    first commit

---
+++ problems/03-make-dog.js
@@ -0,0 +1,27 @@
+class Dog {
+	constructor(name){
+		this.name = name;
+	}
+
+	static makeJet() {
+		return new Dog ("Jet")
+	}
+
+	changeName(newName) {
+		this.name = newName;
+		return this.name;
+	}
+
+	speak(word) {
+		return `${this.name} says ${word}`;
+	}
+}
+
+/*****************************************************************************/
+/***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
+
+try {
+	module.exports = Dog;
+} catch {
+	module.exports = null;
+}
